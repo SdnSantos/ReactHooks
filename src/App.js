a4ff99386283c8ac7a6a5cae58bd1a84a6a6850a
Sidney Santos <sidney_ssantos@hotmail.com>
2019-07-27 14:19:16 -0300

    Modulo 08 - Hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [techs, setTech] = useState(['ReactJS', 'React Native']);
+  const [techs, setTech] = useState([]);
   const [newTech, setNewTech] = useState('');
 
   function handleAdd() {
     setTech([...techs, newTech]);
   }
+
+  //useEffect - substitui os métodos de ciclo de vida
+  useEffect(() => {
+    const storageTech = localStorage.getItem('techs');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
 
   return (
     <>
